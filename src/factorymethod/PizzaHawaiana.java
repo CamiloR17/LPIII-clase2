15d60077f8e25374f06d8a6f22502c1416538a66
CamiloR17 <camilorendon5@gmail.com>
2023-08-26 11:21:11 -0500

    patrones de diseño

---
+++ src/factorymethod/PizzaHawaiana.java
@@ -0,0 +1,19 @@
+package src.factorymethod;
+
+public class PizzaHawaiana implements Pizza{
+
+    @Override
+    public String getTipoMasa() {
+        return "Delgada";
+    }
+
+    @Override
+    public String getTipoQueso() {
+        return "Mozzarella";
+    }
+
+    @Override
+    public String getToppings() {
+        return "Jamón y Piña";
+    }
+}
--- src/package2/Main.java
+++ src/package2/Main.java
@@ -1,14 +1,37 @@
 package src.package2;
 
 import src.builder.Empleado;
+import src.factorymethod.CreadorPizza;
+import src.factorymethod.CreadorPizzaHawaiana;
+import src.factorymethod.CreadorPizzaSalami;
+import src.factorymethod.Pizza;
+import src.prototype.Estudiante;
 
 public class Main {
 
     public static void main(String[] args) {
+        /*
         Empleado empleado = Empleado.builder()
                 .id(123L)
                 .nombre("Jorge")
                 .build();
         System.out.println(empleado);
+        */
+
+        /*
+        Estudiante estudiante = new Estudiante("Camilo", 7656L, 9);
+        Estudiante estudiante2 = estudiante.clone();
+
+        System.out.println(estudiante2 == estudiante);
+        */
+
+        CreadorPizza creadorPizza = new CreadorPizzaSalami();
+        Pizza pizza = crearPizza(creadorPizza);
+
+        System.out.println(pizza.getTipoMasa() + " " + pizza.getTipoQueso() + " " + pizza.getToppings());
+    }
+
+    public static Pizza crearPizza(CreadorPizza creadorPizza) {
+        return creadorPizza.crear();
     }
 }
